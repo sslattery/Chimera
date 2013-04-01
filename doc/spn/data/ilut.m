cfa901ca005b275a945776e7a592fc569e20b698
Stuart Slattery <sslattery@wisc.edu>
2013-04-01 11:09:22 -0500

    adding ILUT preconditioning script for matlab

---
+++ doc/spn/data/ilut.m
@@ -0,0 +1,19 @@
+function [] = ilut( matname, drop_tol )
+% Block-Jacobi preconditioned iteration matrix eigenvalues
+filename = strcat(matname,'.mat');
+A = load(filename,'-ascii');
+A = spconvert(A);
+sizeA = size(A,1);
+
+% Build the block preconditoner matrix
+setup.type = 'ilutp';
+setup.droptol = drop_tol;
+[L,U] = ilu(A,setup);
+
+% Compute the spectral radius
+I = speye(sizeA);
+H = I-(L^(-1))*A*(U^(-1));
+opts.tol=1.0e-8;
+eigs(H,1,'lm',opts)
+
+end
