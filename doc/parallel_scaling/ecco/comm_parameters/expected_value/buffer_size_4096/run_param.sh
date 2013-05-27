74b25a5bda3969b87c21da77434c0b69cbe4dcb0
stuart <stuart@zeep.comm.zeep.ecco>
2013-05-26 19:25:05 -0500

    adding comm parameter study for the ecco cluster

---
+++ doc/parallel_scaling/ecco/comm_parameters/expected_value/buffer_size_4096/run_param.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+qsub param_16.pbs
+qsub param_32.pbs
+qsub param_64.pbs
+qsub param_128.pbs
+qsub param_256.pbs
+qsub param_512.pbs
+qsub param_1024.pbs
+qsub param_2048.pbs
+qsub param_4096.pbs
---
+++ doc/parallel_scaling/ecco/comm_parameters/expected_value/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./buffer_size_16/run_param.sh
+./buffer_size_32/run_param.sh
+./buffer_size_64/run_param.sh
+./buffer_size_128/run_param.sh
+./buffer_size_256/run_param.sh
+./buffer_size_512/run_param.sh
+./buffer_size_1024/run_param.sh
+./buffer_size_2048/run_param.sh
+./buffer_size_4096/run_param.sh
