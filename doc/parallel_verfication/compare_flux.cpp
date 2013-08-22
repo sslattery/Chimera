5f78aa6b609fec31c6d8de252e63c2dbca6b7107
Stuart Slattery <sslattery@wisc.edu>
2013-08-21 23:14:11 -0500

    updating parallel flux comparison to use relative values

---
+++ doc/parallel_verfication/compare_flux.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <cassert>
+
+int main( int argc, char * argv[] )
+{
+    std::string file_1 = "cg_reference.dat";
+    std::string file_2 = "mcsa_1_8_10.dat";
+
+    int global_length;
+
+    std::ifstream data_file_1;
+    data_file_1.open( file_1.c_str() );
+    data_file_1 >> global_length;
+    std::vector<double> global_vector_1( global_length );
+    for ( int i = 0; i < global_length; ++i )
+    {
+        data_file_1 >> global_vector_1[i];
+    }
+    data_file_1.close();
+
+    std::ifstream data_file_2;
+    data_file_2.open( file_2.c_str() );
+    data_file_2 >> global_length;
+    std::vector<double> global_vector_2( global_length );
+    for ( int i = 0; i < global_length; ++i )
+    {
+        data_file_2 >> global_vector_2[i];
+    }
+    data_file_2.close();
+
+    std::vector<double> difference( global_length );
+    for ( int i = 0; i < global_length; ++i )
+    {
+        difference[i] = std::abs( global_vector_1[i] - global_vector_2[i] ) /
+                        std::abs( global_vector_1[i] );
+    }
+
+    std::cout << "Max Difference: " 
+              << *std::max_element( difference.begin(), difference.end() )
+              << std::endl;
+
+    std::cout << "Min Difference: " 
+              << *std::min_element( difference.begin(), difference.end() )
+              << std::endl;
+}
